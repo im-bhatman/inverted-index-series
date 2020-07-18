2674d48a02383c89aa37ab8b3d69589e51ed73c5
bhatman-r <riteshbhat17@gmail.com>
2020-07-19 01:53:20 +0530

    increment counter class to use as composition in index handler on term counting

---
+++ src/main/java/com/bhatmanr/invindex/comparisons/dictionaryimpl/IncrementCounter.java
@@ -0,0 +1,8 @@
+package com.bhatmanr.invindex.comparisons.dictionaryimpl;
+
+public class IncrementCounter {
+    private int counter;
+    public void incrementCounterValue() {
+        counter+=1;
+    }
+}
