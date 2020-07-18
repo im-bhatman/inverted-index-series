e0a05ba5467336d60eeef7145ce30eb043782253
bhatman-r <riteshbhat17@gmail.com>
2020-07-19 01:52:14 +0530

    Subject class implemented with indexhandler

---
+++ src/main/java/com/bhatmanr/invindex/comparisons/dictionaryimpl/IndexHandler.java
@@ -0,0 +1,33 @@
+package com.bhatmanr.invindex.comparisons.dictionaryimpl;
+
+import java.util.ArrayList;
+
+public class IndexHandler implements Subject {
+
+    private ArrayList<DictionaryIndexObserver> indexObservers;
+    private IncrementCounter indexTermCount = new IncrementCounter();
+
+    public IndexHandler() {
+        indexObservers = new ArrayList<DictionaryIndexObserver>();
+        indexTermCount.incrementCounterValue();
+    }
+
+
+    public void RegisterObserverIndex(DictionaryIndexObserver indexObj) {
+        indexObservers.add(indexObj);
+    }
+
+    public void RemoveObserverIndex(DictionaryIndexObserver indexObj) {
+        int i = indexObservers.indexOf(indexObj);
+        if (i >=0) {
+            indexObservers.remove(i);
+        }
+    }
+
+    public void UpdateObserverIndex(String term, String doc_id) {
+        for (DictionaryIndexObserver observer: indexObservers) {
+            observer.UpdateIndex(term, doc_id);
+        }
+    }
+
+}
