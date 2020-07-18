c4a75749daac8a2aff5a6e7fe1c295c7a920cecf
bhatman-r <riteshbhat17@gmail.com>
2020-07-19 01:51:20 +0530

    Old dictionary index interface deleted

--- src/main/java/com/bhatmanr/invindex/comparisons/dictionaryimpl/DictionaryIndex.java
+++
@@ -1,8 +0,0 @@
-package com.bhatmanr.invindex.comparisons.dictionaryimpl;
-
-public interface DictionaryIndex {
-
-    void AddToIndex(String term, String document);
-
-    void SearchTerm(String term);
-}
